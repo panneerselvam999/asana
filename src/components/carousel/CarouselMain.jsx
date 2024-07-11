155c9d9b422d4f190ae7664ba31635411b92289d
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-07-11 12:10:38 +0530

    carousel final

--- src/components/carousel/CarouselMain.jsx
+++ src/components/carousel/CarouselMain.jsx
@@ -11,11 +11,9 @@
 // import { GrLinkNext } from "react-icons/gr";
 // import { GrLinkPrevious } from "react-icons/gr";
 
-
 // const CarouselMain = () => {
 //     const showCount = card.cardData.length;
 //     const slideRef = useRef(null);
-
 
 //     function SampleNextArrow(props) {
 //         const { className, style, onClick } = props;
@@ -94,9 +92,7 @@
 
 // export default CarouselMain;
 
-
 ////////////////////////////////////////////////////////////////////////////////
-
 
 import { FaArrowRight } from "react-icons/fa";
 import React, { useRef } from "react";
@@ -146,20 +142,19 @@ const CarouselMain = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         variableWidth: true,
-        arrows:false,
+        arrows: false,
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />,
     };
 
     return (
-        <section className="w-full bg-[#F3F3F3] px-6  py-6 md:px-10 md:py-8 xl:px-20 xl:py-10 ">
-           
+        <section className="w-full bg-[#F3F3F3] px-6 py-6 pt-12 md:pt-16 xl:pt-20  md:px-10 md:py-8 xl:px-20 xl:py-10">
             <div>
-                <h1 className="text-3xl font-semibold tracking-tighter md:text-6xl">
+                <h1 className="max-w-[800px] text-3xl font-semibold tracking-tighter md:text-6xl">
                     The world’s top companies trust Asana
                 </h1>
                 <p>
-                    <a href="#" className="inline-flex items-center">
+                    <a href="#" className="mt-16 inline-flex items-center font-bold">
                         See all case studies <FaArrowRight className="text-sm" />
                     </a>
                 </p>
@@ -167,13 +162,15 @@ const CarouselMain = () => {
 
             <div>
                 <div className="slider-container">
-                    <Slider ref={slideRef} {...settings}> {/* Changed line */}
+                    <Slider ref={slideRef} {...settings}>
+                        {" "}
+                        {/* Changed line */}
                         {card.cardData.map((data) => (
                             <CarouselCard propData={data} key={data.id} />
                         ))}
                     </Slider>
                 </div>
-                <div className=" flex gap-4">
+                <div className="flex gap-4">
                     <button
                         onClick={() => slideRef.current.slickPrev()}
                         className="rounded-full border border-slate-300 p-2 duration-300 hover:bg-slate-300"
